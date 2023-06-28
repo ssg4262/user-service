00db3b53384b708094d4edb315b15fa4f5c0c60a
koSeungBeom <rhtmdqja275@naver.com>
2023-06-28 10:58:48 +0900

    entity,repository 추가

---
+++ src/main/java/com/oxog/userservice/Entity/UserEntity.java
@@ -0,0 +1,21 @@
+package com.oxog.userservice.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+// db 테이블 변환
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// 자동 생성
+    private Long id;
+    @Column(nullable = false, length = 50)
+    private String email;
+    @Column(nullable = false, length = 50)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String userId;
+    @Column(nullable = false, unique = true)
+    private String encryptedPwd;
+}
---
+++ src/main/java/com/oxog/userservice/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.oxog.userservice.repository;
+
+import com.oxog.userservice.Entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity,Long> {
+}
