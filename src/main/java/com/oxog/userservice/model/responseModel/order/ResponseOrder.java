9b21338d06528c53562dee6709c15102c8829cb6
koSeungBeom <rhtmdqja275@naver.com>
2023-06-28 15:27:17 +0900

    주문정보 모델 추가

--- src/main/java/com/oxog/userservice/model/responseModel/order/ResponseOrder.java
+++ src/main/java/com/oxog/userservice/model/responseModel/order/ResponseOrder.java
@@ -1,4 +1,16 @@
 package com.oxog.userservice.model.responseModel.order;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class ResponseOrder {
+    private String productId; // 상품아이디
+    private Integer Qty; //수량
+    private Integer unitPrice; // 개당가격
+    private Integer totalPrice; // 총 가격
+    private Date createdAt;
+
+    private String orderId;// 주문자 아이디
 }
