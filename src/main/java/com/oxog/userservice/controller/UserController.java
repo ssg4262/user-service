803ab883db1e504546ac0fb50af77d8475f6a219
koSeungBeom <rhtmdqja275@naver.com>
2023-06-28 11:35:17 +0900

    회원가입 로직 추가

--- src/main/java/com/oxog/userservice/controller/UserController.java
+++ src/main/java/com/oxog/userservice/controller/UserController.java
@@ -1,21 +1,28 @@
 package com.oxog.userservice.controller;
 
+import com.oxog.userservice.model.UserModel;
+import com.oxog.userservice.model.requestModel.RequestUser;
+import com.oxog.userservice.service.userSevice.UserService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
-    private Environment env;
 
-    public UserController(Environment env) {
-        this.env = env;
-    }
+    @Autowired
+    private UserService userService;
 
-    @GetMapping("/health_check")
-    public String status(){
-        return "connect"+env.getProperty("greeting.message");
+    ModelMapper mapper = new ModelMapper();
+
+    @PostMapping("")
+    public String createUser(@RequestBody RequestUser user){
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserModel userModel = mapper.map(user , UserModel.class);
+        userService.createUser(userModel);
+        return "success";
     }
 }
