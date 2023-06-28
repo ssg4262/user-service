6abc9bd397b261f1e1b733947329a328e28e37e4
koSeungBeom <rhtmdqja275@naver.com>
2023-06-28 13:13:37 +0900

    컨트롤러 응답 변경, 응답객체 추가

---
+++ src/main/java/com/oxog/userservice/model/responseModel/ResponseUser.java
@@ -0,0 +1,10 @@
+package com.oxog.userservice.model.responseModel;
+
+import lombok.Data;
+
+@Data
+public class ResponseUser {
+    private String email;
+    private String name;
+    private String userId;
+}
