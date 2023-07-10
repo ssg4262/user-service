29a369027f5dc87cfa30824c0759d3386312672a
koSeungBeom <rhtmdqja275@naver.com>
2023-07-10 14:46:16 +0900

    로그인시 삭제한 회원은 로그인불가

--- src/main/java/com/oxog/userservice/security/AuthenticationFilter.java
+++ src/main/java/com/oxog/userservice/security/AuthenticationFilter.java
@@ -10,6 +10,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,7 +22,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
-
+@Log4j2
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     private UserService userService;
@@ -59,15 +60,16 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             Authentication authResult) throws IOException, ServletException { // 인증 성공시
             String userEmail = ((User)authResult.getPrincipal()).getUsername();
             UserModel userModel = userService.getUserByEmail(userEmail);
+            if(!userModel.getDeleteYn().equals("Y")){
+                String token = Jwts.builder()
+                        .setSubject(userModel.getUserId())
+                        .setExpiration(new Date(System.currentTimeMillis() +
+                                Long.parseLong(env.getProperty("token.expiration_time")))) // yaml에 정의한 속성
+                        .signWith(SignatureAlgorithm.HS512,env.getProperty("token.secret"))
+                        .compact();
 
-        String token = Jwts.builder()
-                .setSubject(userModel.getUserId())
-                .setExpiration(new Date(System.currentTimeMillis() +
-                        Long.parseLong(env.getProperty("token.expiration_time")))) // yaml에 정의한 속성
-                .signWith(SignatureAlgorithm.HS512,env.getProperty("token.secret"))
-                .compact();
-
-        response.addHeader("token" , token);
-        response.addHeader("userId" , userModel.getUserId());
+                response.addHeader("token" , token);
+                response.addHeader("userId" , userModel.getUserId());
+            }
     }
 }
