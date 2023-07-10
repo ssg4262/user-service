4516088ba7d6acc45cd173a684d7a497d44ff361
koSeungBeom <rhtmdqja275@naver.com>
2023-07-10 15:44:23 +0900

    삭제 후 처리 추가 실질적으로 삭제되진않음 플레그이용

--- src/main/java/com/oxog/userservice/repository/UserRepository.java
+++ src/main/java/com/oxog/userservice/repository/UserRepository.java
@@ -10,6 +10,10 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<UserEntity,Long> {
     @Query("SELECT u FROM UserEntity u WHERE u.deleteYn = 'N'")
     Optional<List<UserEntity>> findAllByDeleteYn();
+    @Query("SELECT u FROM UserEntity u WHERE u.userId = :userId AND u.deleteYn = 'N'")
     UserEntity findByUserId(String userId);
+
+    @Query("SELECT u FROM UserEntity u WHERE u.email = :email AND u.deleteYn = 'N'")
     UserEntity findByEmail(String email);
+
 }
