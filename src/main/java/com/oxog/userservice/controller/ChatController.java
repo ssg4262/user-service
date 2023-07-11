fe84b50b594a90e190507e971702313a41ea3cb1
koSeungBeom <rhtmdqja275@naver.com>
2023-07-11 15:53:24 +0900

    웹소캣으로 수정중

--- src/main/java/com/oxog/userservice/controller/ChatController.java
+++ src/main/java/com/oxog/userservice/controller/ChatController.java
@@ -4,6 +4,8 @@ import com.oxog.userservice.model.requestModel.chat.ChatUser;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 @RequestMapping("/")
 public class ChatController {
+    private final SimpMessageSendingOperations messagingTemplate;
 
-    @MessageMapping("/setUser")
-    public void setUser(ChatUser chatUser, SimpMessageHeaderAccessor headerAccessor) {
-        log.info(headerAccessor.getUser().getName());
+    public ChatController(SimpMessageSendingOperations messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
     }
+
+    @MessageMapping("/sendMessage")
+    public void sendMessage(ChatUser message, SimpMessageHeaderAccessor headerAccessor) {
+        // Handle the received message
+        log.info("Received message: {}", message.getContent());
+
+        // Broadcast the message to all connected clients
+        messagingTemplate.convertAndSend("/topic/chat", message);
+    }
+
 
 }
--- src/main/java/com/oxog/userservice/model/requestModel/chat/ChatUser.java
+++ src/main/java/com/oxog/userservice/model/requestModel/chat/ChatUser.java
@@ -1,4 +1,9 @@
 package com.oxog.userservice.model.requestModel.chat;
 
+import lombok.Data;
+
+@Data
 public class ChatUser {
+    private String sender;
+    private String content;
 }
